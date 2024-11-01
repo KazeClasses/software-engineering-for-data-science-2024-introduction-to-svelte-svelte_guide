0b0340ad8804e4afd9ee1d613697dd6bd352087c
EricAlanSimon <esimon10@jhu.edu>
2024-10-31 20:17:33 -0400

    Svelte tutorial and attendance

---
+++ src/lib/components/Hello.svelte
@@ -0,0 +1,20 @@
+<script>
+  let name = 'world';
+  let pokeCount = 0;
+
+  function handlePoke() {
+    pokeCount += 1;
+  }
+</script>
+
+<input bind:value={name} placeholder="Enter your name" />
+<p>Hello {name}!</p>
+
+<button on:click={handlePoke}>
+  Poked {pokeCount} {pokeCount === 1 ? 'time' : 'times'}
+</button>
+
+{#if pokeCount > 10}
+  <p>You are annoying</p>
+{/if}
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+ import Navbar from '/Users/ericsimon/SweDS/introduction-to-svelte-EricAlanSimon/src/components/Navbar.svelte';
+</script>
+<Navbar />
